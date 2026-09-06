be33fdbf1b5ff647f76287d4552ee389f9b24c0e
David Eccles (gringer) <github@gringer.org>
2026-09-06 18:42:42 +1200

    Update 3D Honeycomb to alternate layer directions on square tops
    The updated 3D Honeycomb infill now only changes direction at the smallest
    crossing point. This substantially improves pattern strength and removes the
    need for bridge infill.
    
    Additional fixes:
    * Determine exact critical points for straight lines
    * Bounds adjustment to make sure the full pattern is printed
    * Pattern density tweaks (to make infill % better match extruded density for a 30mm cube)
    * Fill in virtual functions, as per @pmarks-net's suggestions [https://github.com/prusa3d/PrusaSlicer/pull/6434#issuecomment-3192702236]
    * Backport rotation support from OrcaSlicer
    * Fix up variable types and bounding box operations to be compatible with v3.0.0

--- src/libslic3r/src/libslic3r/Fill/Fill3DHoneycomb.hpp
+++ src/libslic3r/src/libslic3r/Fill/Fill3DHoneycomb.hpp
@@ -17,8 +17,9 @@ public:
     Fill* clone() const override { return new Fill3DHoneycomb(*this); };
     ~Fill3DHoneycomb() override {}
 
-	// require bridge flow since most of this pattern hangs in air
-    bool use_bridge_flow() const override { return true; }
+    // note: updated 3D Honeycomb doesn't need bridge flow because the
+    //       pattern is placed on top of previous layers
+    bool use_bridge_flow() const override { return false; }
     bool is_self_crossing() override { return false; }
 
 protected:
@@ -26,7 +27,7 @@ protected:
 	    const FillParams                &params, 
 	    unsigned int                     thickness_layers,
 	    const std::pair<float, Point>   &direction, 
-	    ExPolygon                 		 expolygon,
+	    ExPolygon                        expolygon,
 	    Polylines                       &polylines_out) override;
 };
 
