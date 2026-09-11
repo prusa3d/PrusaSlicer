df53a2edd52000e8a08394f0a870a6be8026be8e
Jan Bařtipán <barzto@gmail.com>
2026-09-11 19:38:12 +0200

    SPE-4103 (#15636): Lua/TempTower: fixed placement to cope with various available fonts

--- resources/lua/com.prusa3d.slicer.calibration/temp_tower.lua
+++ resources/lua/com.prusa3d.slicer.calibration/temp_tower.lua
@@ -9,6 +9,13 @@ info = {
         {name = "steps", label = "Num Steps", type = "int", default = 5}
     }
 }
+
+local function alignment(obj_min, obj_max, obj_factor, target_min, target_max, target_factor)
+    local obj_point = (obj_max - obj_min) * obj_factor + obj_min
+    local target_point = (target_max - target_min) * target_factor + target_min
+    return target_point - obj_point
+end
+
 function execute(opts)
     local base_mesh = api.load_stl("temp_tower-base.stl")
     local base_aabb = base_mesh:bounds()
@@ -32,12 +39,18 @@ function execute(opts)
     bed:print_presets():set("top_solid_layers", 5)
 
     local font = api.get_font("Helvetica")
+    local step_mesh = api.load_stl("temp_tower-step.stl")
+    local step_bounds = step_mesh:bounds()
+
+    local step_text_edge_end_x = -6
+    local text_padding = 1
+    local step_text_end_x = step_text_edge_end_x - text_padding
 
     for i = 1, opts.steps do
         local temp = opts.max_temp - step_temp * (i - 1)
         local z = base_height + step_height * (i - 1) + step_aabb.min_z
         table.insert(other_volumes, {
-            mesh = api.load_stl("temp_tower-step.stl"),
+            mesh = step_mesh,
             translate = {
                 z = z
             }
@@ -47,27 +60,27 @@ function execute(opts)
         local gcode = "M104 S" .. rounded_temp
         api.project:insert_layer_custom_gcode(api.project:current_bed(), z + layer_height, gcode)
 
+        local text_mesh = api.emboss_text { font = font, text = "" .. rounded_temp, depth = 1 }
+        local text_bounds = text_mesh:bounds()
+        local text_offset = alignment(
+            text_bounds.min_y, text_bounds.max_y, 0.5,
+            step_bounds.min_z, step_bounds.max_z, 0.5
+        )
         ---@type VolumeDefinition
         local vol = {
-            mesh = api.emboss_text{
-                font=font,
-                text="" .. rounded_temp,
-                depth=1
-            },
+            mesh = text_mesh,
             type = VolumeType.Negative,
             rotate = {
                 x = 90
             },
             translate = {
-                x = -15,
+                x = step_text_end_x - text_bounds.max_x,
                 y = -4.1,
-                z = z + 3.15
+                z = z + text_offset
             },
         }
 
         table.insert(other_volumes, vol)
-
-
     end
 
     api.project:add_object{
