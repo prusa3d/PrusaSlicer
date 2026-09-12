f684e9ffbceb50701dae1006a902c2846ac2435f
Hernan Martinez <hmartinez82@pm.me>
2026-09-12 14:53:31 -0600

    SDL: Fix ARM64 intrinsics

--- deps/+SDL/SDL.cmake
+++ deps/+SDL/SDL.cmake
@@ -3,6 +3,7 @@ add_cmake_project(
         SDL
         URL https://github.com/libsdl-org/SDL/releases/download/release-2.32.4/SDL2-2.32.4.zip
         URL_HASH SHA256=030bd2768653bf11cefef06aaa99e61e850326871d283b81d324fb7242b6c702
+        PATCH_COMMAND ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/arm64-intrinsics.patch
         CMAKE_ARGS
             -DINCLUDE_INSTALL_DIR=${CMAKE_INSTALL_INCLUDEDIR}
             -DBUILD_SHARED_LIBS=ON
